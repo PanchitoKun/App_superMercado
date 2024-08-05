f27b972ce76b07cc1ea211e23f749a9b89216126
Francisco Duran <149026013+PanchitoKun@users.noreply.github.com>
2024-08-05 03:07:40 -0600

    voy intentar colocar fotos
    para la interfaz de java o i creación no se vea tan mala

--- src/model/Cereales.java
+++ src/model/Cereales.java
@@ -15,6 +15,69 @@ public class Cereales extends Producto{
     private String sabor_cereales; 
     private int kg_cerales; 
  private String marca ; 
+
+    public Cereales() {
+    }
+
+    public Cereales(int id_cereales, int fecha_de_vencimiento, String sabor_cereales, int kg_cerales, String marca) {
+        this.id_cereales = id_cereales;
+        this.fecha_de_vencimiento = fecha_de_vencimiento;
+        this.sabor_cereales = sabor_cereales;
+        this.kg_cerales = kg_cerales;
+        this.marca = marca;
+    }
+
+    public Cereales(int id_cereales, int fecha_de_vencimiento, String sabor_cereales, int kg_cerales, String marca, int id, String nombre, String descripcion, int stock, int precio, String sellos) {
+        super(id, nombre, descripcion, stock, precio, sellos);
+        this.id_cereales = id_cereales;
+        this.fecha_de_vencimiento = fecha_de_vencimiento;
+        this.sabor_cereales = sabor_cereales;
+        this.kg_cerales = kg_cerales;
+        this.marca = marca;
+    }
+
+    public int getId_cereales() {
+        return id_cereales;
+    }
+
+    public void setId_cereales(int id_cereales) {
+        this.id_cereales = id_cereales;
+    }
+
+    public int getFecha_de_vencimiento() {
+        return fecha_de_vencimiento;
+    }
+
+    public void setFecha_de_vencimiento(int fecha_de_vencimiento) {
+        this.fecha_de_vencimiento = fecha_de_vencimiento;
+    }
+
+    public String getSabor_cereales() {
+        return sabor_cereales;
+    }
+
+    public void setSabor_cereales(String sabor_cereales) {
+        this.sabor_cereales = sabor_cereales;
+    }
+
+    public int getKg_cerales() {
+        return kg_cerales;
+    }
+
+    public void setKg_cerales(int kg_cerales) {
+        this.kg_cerales = kg_cerales;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+ 
+ 
+ 
     
     
     
